1c377d248949cfa0399c3427315efd9daa5896eb
imac <1>
2020-12-19 00:37:17 +0200

    домашка

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-
-print("1111111", "2222222", sep="!!!!!!!!!")
-print("privet mir")
-
+if __name__ == '__main__':
+    s = "ку"
+    print(s*2, sep="-")
+    print("My name is igor and I Should write my first program", end="!!!")
+    #тут будет мой первый комментарий. комментировать пока нечего
+    print("мне скоро")
+    print(10+10+8,"лет")
+    print("родился в 22/12/1992", end="\n\n\n")
+    #проверяю абзацы
+    print("space check", end="\n\n\n")
+    print("The Zen of Python, by Tim Peters \nBeautiful is better than ugly.\nExplicit is better than implicit.\nSimple is better than complex.\nComplex is better than complicated.\nFlat is better than nested.\nSparse is better than dense.\nReadability counts.\nSpecial cases aren't special enough to break the rules.\nAlthough practicality beats purity.\nErrors should never pass silently.\nUnless explicitly silenced.\nIn the face of ambiguity, refuse the temptation to guess.\nThere should be one-- and preferably only one --obvious way to do it.\nAlthough that way may not be obvious at first unless you're Dutch.\nNow is better than never.\nAlthough never is often better than *right* now.\nIf the implementation is hard to explain, it's a bad idea.\nIf the implementation is easy to explain, it may be a good idea.\nNamespaces are one honking great idea -- let's do more of those!")
