753ed202c00042ac7f28078d7a78dea69f0245c9
imac <1>
2020-12-19 02:57:06 +0200

    домашка

---
+++ lesson2.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+#lesson 2
+#task 1
+
+
+<name> = "Igor"
+<day> = "19December"
+print(“Good day <name>! <day> is a perfect day to learn some python.”)
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-if __name__ == '__main__':
-    s = "ку"
-    print(s*2, sep="-")
-    print("My name is igor and I Should write my first program", end="!!!")
-    #тут будет мой первый комментарий. комментировать пока нечего
-    print("мне скоро")
-    print(10+10+8,"лет")
-    print("родился в 22/12/1992", end="\n\n\n")
-    #проверяю абзацы
-    print("space check", end="\n\n\n")
-    print("The Zen of Python, by Tim Peters \nBeautiful is better than ugly.\nExplicit is better than implicit.\nSimple is better than complex.\nComplex is better than complicated.\nFlat is better than nested.\nSparse is better than dense.\nReadability counts.\nSpecial cases aren't special enough to break the rules.\nAlthough practicality beats purity.\nErrors should never pass silently.\nUnless explicitly silenced.\nIn the face of ambiguity, refuse the temptation to guess.\nThere should be one-- and preferably only one --obvious way to do it.\nAlthough that way may not be obvious at first unless you're Dutch.\nNow is better than never.\nAlthough never is often better than *right* now.\nIf the implementation is hard to explain, it's a bad idea.\nIf the implementation is easy to explain, it may be a good idea.\nNamespaces are one honking great idea -- let's do more of those!")
-    print()
+def subtraction(args):
+    pass
 
+
+if __name__ == '__main__':
+    # LESSON 1
+    # task 1
+    s = "ку"
+    print(s * 2, sep="-")
+    print("My name is igor and I Should write my first program", end="!!!")
+    # тут будет мой первый комментарий. комментировать пока нечего
+    print("мне скоро")
+    print(10 + 10 + 8, "лет")
+    print("родился в 22/12/1992", end="\n\n\n")
+    # проверяю абзацы
+    print("space check", end="\n\n\n")
+    print(
+        "The Zen of Python, by Tim Peters \nBeautiful is better than ugly.\nExplicit is better than implicit.\nSimple is better than complex.\nComplex is better than complicated.\nFlat is better than nested.\nSparse is better than dense.\nReadability counts.\nSpecial cases aren't special enough to break the rules.\nAlthough practicality beats purity.\nErrors should never pass silently.\nUnless explicitly silenced.\nIn the face of ambiguity, refuse the temptation to guess.\nThere should be one-- and preferably only one --obvious way to do it.\nAlthough that way may not be obvious at first unless you're Dutch.\nNow is better than never.\nAlthough never is often better than *right* now.\nIf the implementation is hard to explain, it's a bad idea.\nIf the implementation is easy to explain, it may be a good idea.\nNamespaces are one honking great idea -- let's do more of those!")
+    print()
+    # task 2
     a = ("#\t\t#")
     b = ("#########")
     c = ("#########\n")
@@ -24,3 +31,44 @@ if __name__ == '__main__':
     print(b)
     print(a)
     print(a)
+
+    # LESSON 2
+    # task 1 GREETING PROGRAM
+
+    name = "Igor"
+    day = "19 December"
+    print('Good day {0}!\n {1} is a perfect day to learn some python'.format(name, day))
+
+    # task 3 calculator
+
+    a = 45
+    b = 23
+    c = 2
+    print("addition")
+    print(a + b + c)
+    print("subtraction")
+    print(a - b - c)
+    print("division")
+    print(a / b / c)
+    print("multiplication")
+    print(a * b * c)
+    print("exponentiation")
+    print(a ** b ** c)
+    print("modulus")
+    print(a % b)
+    print("Floor division")
+    print(a // b)
+
+print()
+print()
+print()
+print()
+
+# task 2 Greeting
+first_name = "Igor"
+second_name = "Alergush"
+print("Hello" + first_name +  second_name )
+
+
+
+
