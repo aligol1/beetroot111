8e86b90e14739af0bcdacbf08635e9a7ee04e071
Igor <igoralergush@gmail.com>
2020-12-25 17:10:58 +0200

    задание со звездочкой ко 2 уроку

--- lesson3.py
+++ lesson3.py
@@ -57,3 +57,18 @@ if __name__ == '__main__':
 
     # lesson 3
     # задание со звезочкой 1
+phonenumber = input("Введите номер телефона: ")
+    phonenumber = phonenumber.lstrip("+38")
+    phonenumber = phonenumber.replace(" ", "")
+    phonenumber = phonenumber.replace("-", "")
+
+    print(phonenumber)
+    if phonenumber.isdigit():
+        if len(phonenumber) <= 9:
+            print("Мало Цифр")
+        elif len(phonenumber) >= 11:
+            print("Много цифр")
+        else:
+            print("Спасибо.Номер принят")
+    else:
+        print("Телефон должен состоять из цифр")
--- lesson4.py
+++ lesson4.py
@@ -1,22 +1,5 @@
-# lesson 3
-    # задание со звезочкой 1
 def subtraction(args):
     pass
 
 
 if __name__ == '__main__':
-    phonenumber = input("Введите номер телефона: ")
-    phonenumber = phonenumber.lstrip("+38")
-    phonenumber = phonenumber.replace(" ", "")
-    phonenumber = phonenumber.replace("-", "")
-
-    print(phonenumber)
-    if phonenumber.isdigit():
-        if len(phonenumber) <= 9:
-            print("Мало Цифр")
-        elif len(phonenumber) >= 11:
-            print("Много цифр")
-        else:
-            print("Спасибо.Номер принят")
-    else:
-        print("Телефон должен состоять из цифр")
