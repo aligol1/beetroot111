4f2c8658bcaa2b4ab0f2a568bacb2e481c5dbbcb
Igor <igoralergush@gmail.com>
2021-01-04 20:59:27 +0200

    7 урок

---
+++ lesson7.py
@@ -0,0 +1,24 @@
+if __name__ == '__main__':
+    '''Lesson 7 Task 1  A simple function.
+    Create a simple function called favorite_movie, which takes a string containing the name of your favorite movie. 
+    The function should then print “My favorite movie is named {name}”.'''
+    '''
+    def favorite_movie(movie):
+        print("My favorite name is: ", movie)
+
+    name = input("what is your favorite movie?: ")
+    favorite_movie(name)
+    '''
+    '''Lesson 7 Task 2
+    Creating a dictionary. 
+    Create a function called make_country, which takes in a country’s name and capital as parameters. 
+    Then create a dictionary from those parameters, with ‘name’ and ‘capital’ as keys. 
+    Make the function print out the values of the dictionary to make sure that it works as intended.
+    '''
+
+
+
+
+
+
+
