83df73f8fe755778820142477924cc3a860974bf
Igor <igoralergush@gmail.com>
2021-01-08 09:43:10 +0200

    7 урок

---
+++ lesson 8.py
@@ -0,0 +1,48 @@
+
+    '''Lesson 8 Task 1
+Write a function called oops that explicitly raises an IndexError exception when called.
+Then write another function that calls oops inside a try/except state­ment to catch the error. 
+What happens if you change oops to raise KeyError instead of IndexError?
+'''
+def oops(anyerr):
+    if anyerr == IndexError:
+        raise anyerr
+
+
+def itry():
+    strtry = input("print here:")
+    try:
+        for i in range(len(strtry)+1):
+            print(strtry[i])
+    except IndexError:
+        err = IndexError
+        oops(err)
+    else:
+        print('something wrong')
+    finally:
+        print('ok')
+
+if __name__ == '__main__':
+    itry()
+
+'''Lesson 8 Task 2
+Write a function that takes in two numbers from the user via input(), call the numbers a and b,
+and then returns the value of squared a divided by b, construct a try-except block which raises
+an exception if the two values given by the input function were not numbers, 
+and if value b was zero (cannot divide by zero).   
+'''
+def devide(a, b):
+    try:
+        di = int(a)**2/int(b)
+        print('%.2f' % di)
+    except ValueError:
+        print('use numbers')
+        raise
+    except ZeroDivisionError:
+        print('dont use zero')
+        raise
+
+
+a = input('print divid. to squere')
+b = input('enter divider')
+devide(a, b)
--- lesson7.py
+++ lesson7.py
@@ -2,13 +2,13 @@ if __name__ == '__main__':
     '''Lesson 7 Task 1  A simple function.
     Create a simple function called favorite_movie, which takes a string containing the name of your favorite movie. 
     The function should then print “My favorite movie is named {name}”.'''
-    '''
+
     def favorite_movie(movie):
         print("My favorite name is: ", movie)
 
     name = input("what is your favorite movie?: ")
     favorite_movie(name)
-    '''
+
     '''Lesson 7 Task 2
     Creating a dictionary. 
     Create a function called make_country, which takes in a country’s name and capital as parameters. 
@@ -16,7 +16,41 @@ if __name__ == '__main__':
     Make the function print out the values of the dictionary to make sure that it works as intended.
     '''
 
+    def make_country(country, capital):
+        country_dictation = {'country': country, 'capital': capital}
+        return country_dictation
+    def print_dictat(d):
+        print(d.values())
+    if __name__=="__main__":
+        dat = make_country("Ukraine", "Kiev")
+        print_dictat(dat)
 
+
+'''Lesson 7 Task 3 A simple calculator.
+Create a function called make_operation, which takes in a simple arithmetic 
+operator as a first parameter (to keep things simple let it only be ‘+’, ‘-’ or ‘*’) 
+and an arbitrary number of arguments (only numbers) as the second parameter.
+Then return the sum or product of all the numbers in the arbitrary parameter. For example:
+the call make_operation(‘+’, 7, 7, 2) should return 16
+the call make_operation(‘-’, 5, 5, -10, -20) should return 30
+the call make_operation(‘*’, 7, 6) should return 42  '''
+
+
+    def make_operation(a_operator, num1, *args):
+        return_value = num1
+        if a_operator in ('+', '-', '*'):
+            for number in args:
+                if a_operator == '+':
+                    return_value += number
+                elif a_operator == '-':
+                    return_value -= number
+                else:
+                    return_value *= number
+            print(return_value)
+        else:
+            print("operator is wrong, please use + - or * ")
+
+        make_operation('+', 5, 10)
 
 
 
